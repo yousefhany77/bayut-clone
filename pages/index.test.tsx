f07b72be58cd448f0678ce7ec0cad595b9f734ff
yousefhany77 <yousefhany2014@gmail.com>
2022-10-14 18:43:29 +0200

    first

---
+++ pages/index.test.tsx
@@ -0,0 +1,13 @@
+import Home from ".";
+import { render, screen } from "@testing-library/react";
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /hello/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
