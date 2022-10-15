63d708cfa396fd1b7c3b722787dbdd416bc46102
yousefhany77 <yousefhany2014@gmail.com>
2022-10-15 16:12:41 +0200

    searchbox unit test

--- components/search/SearchBox.tsx
+++ components/search/SearchBox.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function SearchBox() {
-  return <div>SearchBox</div>;
+  return (
+    <div>
+      <h1>search around</h1>
+    </div>
+  );
 }
 
 export default SearchBox;
--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,8 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
